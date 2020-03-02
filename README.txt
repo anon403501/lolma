824d9f9ecdbad4560fa985ccfe33ef689eff487f
anon403501 <61645977+anon403501@users.noreply.github.com>
2020-03-02 17:02:36 +0000

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,14 @@
+hello !!
+
+I started learning C++ a couple of mounths ago and i made this application as my first project.
+
+I know it's trash compared to other projects and that it can be done in much better ways, the whole project idea was about making a virtual assitant like google assitant or alexa.
+
+This app is just as semple as putting the questions in an array and the answers in an array, and if the app don't know the answer to the question it will ask you for a suggesion of the question, for now i could not manage to put the answers and questions in a file and save them separatly so when you close the app all the info the you added will just go (BTW i made it in only 3 hours in total and i know that's alot for such a simple app)
+
+One of the problems i found with this app is that the user can't input spaces in the answers or questions i don't know the reason but i am sure it's really easy to solve.
+
+And i am thinking of adding a text to speak library and speak to text to make it more like an actual virtual assistant and in the futere i would like to go online with this app to let everyone colaberate in it (i know there are apps like this already i just want to make my own version).
+
+I know this is kinda dissapointing but i am really proud of what i have done so far, i hope you like it.
+of course i only posted this app to get suggesions from people to help improve my self in the future.
---
+++ main.cpp
@@ -0,0 +1,123 @@
+//includes
+#include <iostream>
+#include <istream>
+#include <string>
+using namespace std;
+
+//variables
+string question = "unknown";
+string question2[99]; // the questions stored in the program
+string answer2[99]; // the answers stored in the program
+string answer = "donno";
+string YorN = "no";
+int counter = 1; // to sync the answer location with the question location
+int qnaCounter; // this is used to check all the plases in the answers and questions arrays in GetAnswer()
+bool qnaExist = false; // to tell if the question exists in the array
+
+int main()
+{
+    //functions pre declarations
+    string GetAnswer(string ques);
+    void intro(); // this one is just outputs text
+
+////YOU STOPPED HERE
+
+    //ready answers
+    //i put them at the end of the array so new entries wont replace them
+    question2[94] = "hello";
+    answer2[94] = "hi_how_are_you";
+
+    question2[95] = "fine";
+    answer2[95] = "good_:)";
+
+    question2[96] = "how_are_you";
+    answer2[96] = "i_am_great";
+
+    question2[97] = "hi";
+    answer2[97] = "hello";
+
+    question2[98] = "yo";
+    answer2[98] = "sup";
+
+    question2[99] = "test";
+    answer2[99] = "IT'S_ALIVE_!!!";
+
+    //main code
+    intro();
+    for (int f = 0; f < 1000; f++)
+    {
+        cout << "ask me a question : " << flush;
+        cin >> question;
+        GetAnswer(question);
+        if (answer == "donno")
+        {
+            cout << "i can't figure out the answer for this question" << endl;
+            cout << "do you have an answer for this question (reply with yes or no) : "<< flush;
+            cin >> YorN;
+            if (YorN == "yes" || YorN =="Yes" || YorN == "y" || YorN == "Y")
+            {
+                cout << "suggest an answer : " << flush;
+                cin >> answer2[counter];
+                cout << endl << "i will put that in mind... thanks" << endl;
+                question2[counter] = question;
+                counter++;
+            }
+            else
+            {
+                cout << "what ever you see... thanks anyway" << endl;
+            }
+        }
+        else
+        {
+            if (answer == "")
+            {
+                cout << "answer = none (this is an error)" << endl;
+            }
+            cout << answer << endl;
+        }
+        answer = "donno";
+    }
+    //testes
+    /*
+    cout << "..........tests.........." << endl;
+    cout << answer2[3] << endl;
+    cout << question2[3] << endl;
+    */
+
+    //don't touch
+    cout << endl << endl;
+    return 0;
+}
+
+
+//functions
+string GetAnswer(string ques)
+{
+
+    for (int i = 0; i < 101; i++)
+    {
+        if (question == question2[i])
+        {
+            qnaExist = true;
+            qnaCounter = i;
+            answer = answer2[qnaCounter];
+            /*
+            cout << "answer = " << answer << endl;
+            cout << "answer2[qnaCounter] = " << answer2[qnaCounter] << endl;
+            cout << "question2[qnaCounter] = " << question2[qnaCounter] << endl;
+            */
+            break;
+        }
+    }
+
+    return answer;
+}
+
+void intro()
+{
+    cout << "Hello and welcome" << endl;
+    cout << "i am your virtual assistant lolma" << endl;
+    cout << "i am stil learning right now so i hope you can help me out a bit" << endl;
+    cout << "if you ask me a question that i don't know the answer for you can type the answer that i was supposed to say" << endl;
+    cout << "that's enough for now..." << endl;
+}
